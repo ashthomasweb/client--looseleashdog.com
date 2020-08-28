50888c703d6f473c596986a7980d8d431af9ba3d
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-08-28 13:30:09 -0400

    added heroku dependencies

--- app.js
+++ app.js
@@ -81,7 +81,13 @@ app.get('/contact', function (req, res) {
 
 
 
+/* --- Heroku --- */
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+  console.log('Server started on port 3000.');
+}
+
 /* --- Localhost --- */
-app.listen(3000, function () {
-    console.log('Server started on port 3000.');
+app.listen(port, function(){
 });
