4b31c25fa52fbae6133bf2f3016758d758c199af
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-10-12 21:37:44 -0400

    started refactor. removed trash code

--- app.js
+++ app.js
@@ -5,21 +5,9 @@ const ejs = require('ejs');
 const favicon = require('express-favicon');
 var Prismic = require('prismic-javascript');
 var PrismicDOM = require('prismic-dom');
-
-
-
-
-var linkResolver = function (doc) {
-    // Pretty URLs for known types
-    if (doc.type === 'blog') return "/post/" + doc.uid;
-    if (doc.type === 'page') return "/" + doc.uid;
-    // Fallback for other types, in case new custom types get created
-    return "/";
-}
-
+var prismicEndpoint = 'https://ashleyth-test.prismic.io/api/v2';
 
 const app = express();
-var apiEndpoint = 'https://ashleyth-test.prismic.io/api/v2';
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set('view engine', 'ejs');
@@ -29,10 +17,20 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
+// Prismic
+// Pretty URLs for known types
+var linkResolver = function (doc) {
+    if (doc.type === 'blog') return "/post/" + doc.uid;
+    if (doc.type === 'page') return "/" + doc.uid;
+    // Fallback for other types, in case new custom types get created
+    return "/";
+}
+
 // Middleware to inject prismic context
 app.use(function (req, res, next) {
     res.locals.ctx = {
-        endpoint: apiEndpoint,
+        endpoint: prismicEndpoint,
         linkResolver: linkResolver,
     };
     // add PrismicDOM in locals to access them in templates.
@@ -40,23 +38,20 @@ app.use(function (req, res, next) {
     next();
 });
 
-
 // Initialize the prismic.io api
 function initApi(req) {
-    return Prismic.getApi(apiEndpoint, {
+    return Prismic.getApi(prismicEndpoint, {
         req: req
     });
 }
 
-
-/* --- ejs Routes --- */
+/* --- Routes --- */
 
 app.get('/', function (req, res) {
     res.render('home', {
         pageTitle: 'Home',
     });
 });
-
 
 app.get('/about', function (req, res) {
     res.render('about', {
@@ -69,20 +64,6 @@ app.get('/services', function (req, res) {
         pageTitle: "Services",
     });
 });
-
-
-app.get('/photos', function (req, res) {
-    res.render('photos', {
-        pageTitle: "Photos",
-    });
-});
-
-app.get('/contact', function (req, res) {
-    res.render('contact', {
-        pageTitle: "Contact",
-    });
-});
-
 
 app.get('/blog', function (req, res) {
     initApi(req).then(function (api) {
@@ -98,7 +79,6 @@ app.get('/blog', function (req, res) {
     });
 });
 
-
 app.get('/posts/:postTitle', function (req, res) {
     let url = './posts/' + req.params.postTitle;
     initApi(req).then(function (api) {
@@ -107,8 +87,6 @@ app.get('/posts/:postTitle', function (req, res) {
         ).then(function (response) {
             // response is the response object. Render your views here.
             if (url) {
-
-
                 res.render('post', {
                     document: response.results,
                     pageTitle: "Blog",
@@ -119,16 +97,23 @@ app.get('/posts/:postTitle', function (req, res) {
     });
 });
 
+app.get('/photos', function (req, res) {
+    res.render('photos', {
+        pageTitle: "Photos",
+    });
+});
 
-/* --- Heroku --- */
+app.get('/contact', function (req, res) {
+    res.render('contact', {
+        pageTitle: "Contact",
+    });
+});
+
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
     console.log('Server started on port 3000.');
 }
 
-/* --- Localhost --- */
 app.listen(port, function () {});
 
-
-// app.listen(0, () => {});
