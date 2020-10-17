35027814bd2be79174112f3f335911261bcc33ff
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-10-17 03:46:58 -0400

    home nave styled

--- public/js/index.js
+++ public/js/index.js
@@ -7,8 +7,7 @@
         if (scrollCount < 2100) {
             document.getElementById("scroll-pane").scrollTo((scrollCount + 300), 0);
             scrollCount = scrollCount + 300;
-        } else {
-        }
+        } else {}
     }
 
     function scrollWinLeft() {
@@ -23,17 +22,18 @@
 // || navbar active link styling
 
 function currentPage() {
-    
+
     const links = Array.from(document.getElementsByClassName("nav-link"));
-    
-    links.forEach(checkActive)
+
+    links.forEach(checkActive);
+
     function checkActive(link) {
         let currentPage = window.location.href;
         if (currentPage == link.firstChild.parentNode.parentNode.href) {
             link.style.paddingBottom = "4px";
             link.style.borderBottom = "5px solid var(--theme-yellow)";
             link.style.color = "var(--theme-yellow)";
-        }        
+        }
     }
     // console.log(window.location.href);
     // console.log(links);
@@ -41,3 +41,36 @@ function currentPage() {
 currentPage();
 
 
+// || homepage link box styles
+
+function homeLinkStyle() {
+    const cardLinks = Array.from(document.getElementsByClassName("card--home"));
+    cardLinks.forEach(addHover);
+
+    function addHover(card) {
+        card.addEventListener("mouseover", functionA)
+        card.addEventListener("mouseout", functionB)
+
+        function functionA(card) {
+            console.log(this);
+            this.classList.add("card--home-hoverjs");
+                        
+            // card.relatedTarget.children[0].h2.style.color = "red";
+            // card.fromElement.childNodes[1].style.borderBottom = "2px solid var(--theme-borders)";
+
+        }
+
+        function functionB(card) {
+            // console.log(card.srcElement.childNodes[1]);
+            // setTimeout(function(card){ 
+            // }, 1000);
+            this.classList.remove("card--home-hoverjs");
+
+            // card.fromElement.childNodes[1].style.borderBottom = "2px solid var(--theme-lightblue)";
+     
+
+        }
+    }
+}
+
+homeLinkStyle();
