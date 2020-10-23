3c609dafe5aaaea1d55d9bfdbacaa7b8a22d0e32
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-10-23 13:15:31 -0400

    fixed navbar styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 // || navbar active link styling
 
-
 function currentPage() {
 
     const links = Array.from(document.getElementsByClassName("nav-link"));
@@ -45,25 +44,17 @@ homeLinkStyle();
 function juicerFeed() {
 
     setTimeout(function () {
-
+      
         // || Text post error hide
-        let x = document.getElementsByClassName("slick-track");
-        const posts = Array.from(x[0].children);
-        posts.forEach(imageCheck);
+        let x = document.getElementsByClassName("j-stack");
+        console.log(x[0].children);
+        const posts1 = Array.from(x[0].children);
+        const posts2 = Array.from(x[1].children);
+        const posts = posts1.concat(posts2);
 
-        function imageCheck(post) {
-            if (post.firstChild.className == "j-text") {
-                post.style.display = "none";
-            }
-        }
-        // || END Text post error hide 
-
-        // || Hashtag linking in-carosel, 
-
+        // || Hashtag linking 
         posts.forEach(createHashLink);
-
         function createHashLink(post) {
-
             let postString = post.innerHTML;
             let splitString = postString.split("#");
             let tail = splitString[splitString.length - 1];
@@ -77,72 +68,35 @@ function juicerFeed() {
             let replaceTemp = splitString.join();
             let replacementString = replaceTemp.replace(/,/g, "");
             post.innerHTML = replacementString;
+            // overlay listener
             post.addEventListener("click", overlay);
         }
 
-        // || END Hashtag linking in-post and overlay 
-
         // || Instagram link overlay pane
-
         function overlay(e) {
-            // console.log(this); // grabs html element from juicer-generated content
+            // console.log(this); // grabs modified html element from juicer-generated content
             // console.log(this.children[0].children[0].currentSrc); // post img url
             // console.log(this.children[1].children[1].children[0].innerHTML); // post html with links added
             // console.log(this.children[0].attributes.href.value); // link to instagram post
             // console.log(document.getElementById("instaBtn").attributes[3].value); // button link
-          
-            let originalCopy = this.children[1].children[1].children[0].innerHTML;
-            let copy = originalCopy.split("<br>");
-            console.log(copy[0]); // post copy
-            console.log(copy[copy.length-1]); // post hashtags
 
-            let postText = copy[0];
-            let hashtags = copy[copy.length-1];
+            let originalText = this.children[1].children[1].children[0].innerHTML;
+            let splitText = originalText.split("<br>");
+            // dynamic content objects
+            let postText = splitText[0];
+            let hashtags = splitText[splitText.length - 1];
             let url = this.children[0].attributes.href.value;
             let imgSrc = this.children[0].children[0].currentSrc;
-
+            // insert content
             document.getElementById("photo-overlay").style.display = "block";
-
             document.getElementById("photo-overlay-img").src = imgSrc;
             document.getElementById("photo-overlay-text").innerText = postText;
             document.getElementById("photo-overlay-links").innerHTML = hashtags;
             document.getElementById("instaBtn").attributes[3].value = "location.href='" + url + "'";
+
         }
 
-        // || END Instagram link overlay pane 
-
-        // || Arrow icon and slider width
-        const arrows = Array.from(document.getElementsByClassName("slick-arrow"));
-        arrows.forEach(addListener);
-
-        function addListener(item) {
-            item.addEventListener("click", arrowCounter);
-        }
-        let count = 0;
-        arrows[0].style.display = "none";
-
-        function arrowCounter(e) {
-            if (e.srcElement.ariaLabel == "Previous" && count > 0) {
-                count = count - 1;
-            }
-            if (e.srcElement.ariaLabel == "Next" && count < 4) {
-                count = count + 1;
-            }
-            if (count > 0) {
-                arrows[0].style.display = "inline-block";
-            } else {
-                arrows[0].style.display = "none";
-            }
-            if (count > 3) {
-                arrows[1].style.display = "none";
-            } else {
-                arrows[1].style.display = "inline-block";
-            }
-        }
-        // || Arrow icon and slider width END
-
-
-    }, 1000);
+    }, 1500);
 }
 
 juicerFeed();
@@ -150,3 +104,71 @@ juicerFeed();
 function closeOverlay() {
     document.getElementById("photo-overlay").style.display = "none";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // || Arrow icon and slider width
+// let count = 0;
+// const arrows = Array.from(document.getElementsByClassName("slick-arrow"));
+
+// arrows.forEach(addListener);
+// arrows[0].style.display = "none";
+// function addListener(item) {
+//     item.addEventListener("click", arrowCounter);
+// }
+
+// function arrowCounter(e) {
+//     if (e.srcElement.ariaLabel == "Previous" && count > 0) {
+//         count = count - 1;
+//     }
+//     if (e.srcElement.ariaLabel == "Next" && count < 4) {
+//         count = count + 1;
+//     }
+//     if (count > 0) {
+//         arrows[0].style.display = "inline-block";
+//     } else {
+//         arrows[0].style.display = "none";
+//     }
+//     if (count > 3) {
+//         arrows[1].style.display = "none";
+//     } else {
+//         arrows[1].style.display = "inline-block";
+//     }
+// }
+
+
+// clickPositionUp = e.pageX;
+// if (Math.abs(clickPositionUp - clickPosition) < 15) {     
+
+
+
+// || Mouse drag listener prevents overlay
+// let clickPosition = 0;
+// let clickPositionUp = 0;
+// // let slickPosition = "";
+// // let item;
+
+// function trigger(e) {
+//     clickPosition = e.pageX;
+//     console.log("hi")
+//     // console.log(e.path[4].children[0].style.cssText);
+
+//     // slickPosition = e.path[4].children[0].style.left;
+//     // console.log(slickPosition);
+
+
+//     // e.path[4].children[0].style.left = slickPosition + "!important";
+//     // e.path[4].children[0].style.cssText = slickPosition + "!important";
+
+// }
