99b286c22f7551b311efe20573172f7216c7fb6d
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-12-15 00:25:23 -0500

    minor blog slice changes

--- public/js/mquery.js
+++ public/js/mquery.js
@@ -1,34 +1,55 @@
 // || JS Media Queries for "Looseleashdog"
 
 // || Blog main description slice length
+let collection = document.getElementsByClassName("description");
+
+let arr = Array.from(collection.forEach(element => element.innerText));
+console.log(arr);
+let element = document.getElementsByClassName("description");
 
 function adjustDescriptionMid(size) {
-    let collection = document.getElementsByClassName("description");
-    let element = document.getElementsByClassName("description");
     
     if (size.matches) { // If media query matches
         for (let i = 0; i < collection.length; i++ ) {
-            element[i].innerText = collection[i].innerText.slice(0, 10);
-        }
-    } 
-
-}
-
-function adjustDescriptionLarge(large) {
-    let collection = document.getElementsByClassName("description");
-    let element = document.getElementsByClassName("description");
+            element[i].innerText = element[i].innerText.slice(0, 10);
+        } 
     
-    if (large.matches) {
-        for (let i = 0; i < collection.length; i++ ) {
-            element[i].innerText = collection[i].innerText.slice(0, 200);
-        }    
     }
+
 }
 
-var large = window.matchMedia("(max-width: 800px)");
 var size = window.matchMedia("(max-width: 600px)");
 adjustDescriptionMid(size) // Call listener function at run time
-adjustDescriptionLarge(large) // Call listener function at run time
-
 size.addListener(adjustDescriptionMid) // Attach listener function on state changes
-large.addListener(adjustDescriptionLarge) // Attach listener function on state changes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function adjustDescriptionLarge(large) {
+  
+//     if (large.matches) {
+//         for (let i = 0; i < collection.length; i++ ) {
+//             element[i].innerText = collection[i].innerText.slice(0, 200);
+//         }    
+//     }
+// }
+
+// var large = window.matchMedia("(max-width: 800px)");
+// adjustDescriptionLarge(large) // Call listener function at run time
+
+// large.addListener(adjustDescriptionLarge) // Attach listener function on state changes
