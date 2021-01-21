69d33862a74d0a6931010438f028accb0c534e99
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-01-20 22:53:08 -0500

    adds nodemailer template rf&c. live tested

--- nodemailer.js
+++ nodemailer.js
@@ -1,11 +1,10 @@
+// Nodemailer functions and templates for "Looseleashdog" 
+
+// Dependencies
 const nodemailer = require("nodemailer");
-const {
-    user_name,
-    user_email,
-    message,
-} = require('./app.js')
+const { user_name, user_email, message, } = require('./app.js');
 
-
+// Mailer transport object 
 var transporter = nodemailer.createTransport({
     host: 'mi3-ts3.a2hosting.com',
     port: 465,
@@ -16,72 +15,74 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-function userMessage() {
-
-    let a = `<div style='max-width: 100%; padding: 2rem; border: 1px solid lightgrey; border-radius: 12px; margin: 1rem;'>
-    <h2>From:</h2>
-    <div style='padding: 0rem 2rem;'>
-    <p><strong>${user_name}</strong></p>
-    </div>
-    <h2>Email:</h2>
-    <div style='padding: 0rem 2rem;'>
-    <p>${user_email}</p>
-    </div><h2>Message:</h2>
-    <div style='padding: 0rem 2rem;'>
-    <p>${message}</p>
-    </div>
-    </div>`;
-
-    let b = a.replace(/\n/g, "").replace(/\r/g, "");
-
-    return b;
-};
-
-function userAutoReply() {
+// Templates
+function inquiryTemplate() {
 
     // Do not remove backtick
-    let replyTemplate = ` 
+    let inqTemplate = `
 
-    <div style='max-width: 100%; padding: 2rem; border: 1px solid lightgrey; border-radius: 12px; margin: 1rem;'>   
+    <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
+        <h2>Hi Emily, someone is reaching out from your website!</h2>
+            <p>Below is a copy of the email.</p> 
+        <h2>From:</h2>
+            <p style='padding: 0 30px;'><strong>${user_name}</strong></p>
+        <h2>Email:</h2>
+            <p style='padding: 0 30px;'>${user_email}</p>
+        <h2>Message:</h2>
+            <p style='padding: 0 30px;'>${message}</p>
+    </div>
+    
+    `; // Do not remove backtick
+
+    let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+    return output;
+};
+
+function confirmTemplate() {
+
+    // Do not remove backtick
+    let confTemplate = `
+
+    <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
         <h2>Hi ${user_name}, thanks for reaching out to LooseLeashDog!</h2>
             <p>This is an automatic response confirming that your email was sent. I will reach out to you within the next few days. Below is a copy of your email.</p> 
             <p>Remember, this is an automatic email and doesn't accept replys.</p>
         <h2>From:</h2>
-            <p style='padding: 0rem 2rem;'><strong>${user_name}</strong></p>  
+            <p style='padding: 0 30px;'><strong>${user_name}</strong></p>  
         <h2>Email:</h2>
-            <p style='padding: 0rem 2rem;'>${user_email}</p>
+            <p style='padding: 0 30px;'>${user_email}</p>
         <h2>Message:</h2>
-            <p style='padding: 0rem 2rem;'>${message}</p>
+            <p style='padding: 0 30px;'>${message}</p>
     </div>
 
     `; // Do not remove backtick
 
-    let output = replyTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-
+    let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
     return output;  
 };
 
+// Nodemailer email objects
 function mailNewInquiry(user_name, user_email, message) {
     return `{"from": "mailer@looseleashdog.com",
     "to": "rideoutweb@gmail.com",
     "subject": "A person from your website is reaching out!",
-    "html": "${userMessage()}"}`
+    "html": "${inquiryTemplate()}"}`;
 };
 
 function mailConfirmation(user_name, user_email, message) {
     return `{"from": "mailer@looseleashdog.com",
     "to": "${user_email}",
     "subject": "This is your email confirmation from LooseLeashDog!",
-    "html": "${userAutoReply()}"}`
+    "html": "${confirmTemplate()}"}`;
 };
 
+// Object parsing
 let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
 let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
 
+// Exports
 module.exports = {
     transporter,
-    userMessage,
-    userAutoReply,
     inquiry,
-    finalConfirm
+    finalConfirm,
 };
