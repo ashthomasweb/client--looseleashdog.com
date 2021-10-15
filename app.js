632ccc829115468d0a23f64854dd7372e58abd41
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-10-15 19:51:45 -0400

    Initiating live test
    Updates nodemailer to use oAuth2.

--- app.js
+++ app.js
@@ -174,66 +174,66 @@ app.post('/contact', function (req, res) {
     };
 
 
-    // Templates
-    function inquiryTemplate() {
+    // // Templates
+    // function inquiryTemplate() {
 
-        // Do not remove backtick
-        let inqTemplate = `
+    //     // Do not remove backtick
+    //     let inqTemplate = `
 
-        <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
-            <h2>Hi Emily, someone is reaching out from your website!</h2>
-                <p>Below is a copy of the email.</p> 
-            <h2>From:</h2>
-                <p style='padding: 0 30px;'><strong>${user_name}</strong></p>
-            <h2>Email:</h2>
-                <p style='padding: 0 30px;'>${user_email}</p>
-            <h2>Message:</h2>
-                <p style='padding: 0 30px;'>${message}</p>
-        </div>
+    //     <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
+    //         <h2>Hi Emily, someone is reaching out from your website!</h2>
+    //             <p>Below is a copy of the email.</p> 
+    //         <h2>From:</h2>
+    //             <p style='padding: 0 30px;'><strong>${user_name}</strong></p>
+    //         <h2>Email:</h2>
+    //             <p style='padding: 0 30px;'>${user_email}</p>
+    //         <h2>Message:</h2>
+    //             <p style='padding: 0 30px;'>${message}</p>
+    //     </div>
     
-        `; // Do not remove backtick
+    //     `; // Do not remove backtick
 
-        let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-        return output;
-    };
+    //     let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+    //     return output;
+    // };
 
-    function confirmTemplate() {
+    // function confirmTemplate() {
 
-        // Do not remove backtick
-        let confTemplate = `
+    //     // Do not remove backtick
+    //     let confTemplate = `
 
-        <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
-            <h2>Hi ${user_name}, thanks for reaching out to LooseLeashDog!</h2>
-                <p>This is an automatic response confirming that your email was sent. I will reach out to you within the next few days. Below is a copy of your email.</p> 
-                <p>Remember, this is an automatic email and doesn't accept replys.</p>
-            <h2>From:</h2>
-                <p style='padding: 0 30px;'><strong>${user_name}</strong></p>  
-            <h2>Email:</h2>
-                <p style='padding: 0 30px;'>${user_email}</p>
-            <h2>Message:</h2>
-                <p style='padding: 0 30px;'>${message}</p>
-        </div>
+    //     <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
+    //         <h2>Hi ${user_name}, thanks for reaching out to LooseLeashDog!</h2>
+    //             <p>This is an automatic response confirming that your email was sent. I will reach out to you within the next few days. Below is a copy of your email.</p> 
+    //             <p>Remember, this is an automatic email and doesn't accept replys.</p>
+    //         <h2>From:</h2>
+    //             <p style='padding: 0 30px;'><strong>${user_name}</strong></p>  
+    //         <h2>Email:</h2>
+    //             <p style='padding: 0 30px;'>${user_email}</p>
+    //         <h2>Message:</h2>
+    //             <p style='padding: 0 30px;'>${message}</p>
+    //     </div>
 
-        `; // Do not remove backtick
+    //     `; // Do not remove backtick
 
-        let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-        return output;
-    };
+    //     let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+    //     return output;
+    // };
 
-    // Nodemailer email objects
-    function mailNewInquiry(user_name, user_email, message) {
-        return `{"from": "ashthomasweb@gmail.com",
-    "to": "ashthomasweb@gmail.com",
-    "subject": "A person from your website is reaching out!",
-    "html": "${inquiryTemplate()}"}`;
-    };
+    // // Nodemailer email objects
+    // function mailNewInquiry(user_name, user_email, message) {
+    //     return `{"from": "ashthomasweb@gmail.com",
+    // "to": "ashthomasweb@gmail.com",
+    // "subject": "A person from your website is reaching out!",
+    // "html": "${inquiryTemplate()}"}`;
+    // };
 
-    function mailConfirmation(user_name, user_email, message) {
-        return `{"from": "ashthomasweb@gmail.com",
-    "to": "${user_email}",
-    "subject": "This is your email confirmation from LooseLeashDog!",
-    "html": "${confirmTemplate()}"}`;
-    };
+    // function mailConfirmation(user_name, user_email, message) {
+    //     return `{"from": "ashthomasweb@gmail.com",
+    // "to": "${user_email}",
+    // "subject": "This is your email confirmation from LooseLeashDog!",
+    // "html": "${confirmTemplate()}"}`;
+    // };
 
     //emailOptions - who sends what to whom
     const sendEmail = async (emailOptions) => {
@@ -242,35 +242,41 @@ app.post('/contact', function (req, res) {
     };
 
     
-    // Object parsing
-    let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
-    let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
+    // // Object parsing
+    // let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
+    // let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
     
-    // sendEmail(inquiry);
+    sendEmail({
+        subject: "Test",
+        text: "I am alive!",
+        to: "ashthomasweb@gmail.com",
+        from: process.env.EMAIL
+    });
+    
     // sendEmail(finalConfirm);
     // var userInquiry = transporter().sendMail(inquiry);
-    sendEmail(inquiry);
+    // sendEmail(inquiry);
 
 
     // var userConfirm = transporter().sendMail(finalConfirm);
-    sendEmail(finalConfirm);
+    // sendEmail(finalConfirm);
 
-    Promise.all([userInquiry, userConfirm])
-        .then(([resultInq, resultConf]) => {
-            console.log("Emails sent", resultInq, resultConf);
-        })
-        .catch((err) => {
-            console.log(err);
-            console.log('error')
-            ifError = true;
-        })
-        .finally(() => {
-            res.render('contact', {
-                pageTitle: "Contact",
-                responseBool: true,
-                isError: ifError,
-            });
-        });
+    // Promise.all([userInquiry, userConfirm])
+    //     .then(([resultInq, resultConf]) => {
+    //         console.log("Emails sent", resultInq, resultConf);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         console.log('error')
+    //         ifError = true;
+    //     })
+    //     .finally(() => {
+    //         res.render('contact', {
+    //             pageTitle: "Contact",
+    //             responseBool: true,
+    //             isError: ifError,
+    //         });
+    //     });
 
 });
 
