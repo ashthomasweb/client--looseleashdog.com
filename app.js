51624e860cf0ef9ccec46b39ecad299c3737088c
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-10-14 20:29:09 -0400

    Updates /post and packages for heroku
    Adds ssl handler and brings nodemailer into app.

--- app.js
+++ app.js
@@ -4,19 +4,24 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
 const ejs = require('ejs');
 const favicon = require('express-favicon');
 var Prismic = require('prismic-javascript');
 var PrismicDOM = require('prismic-dom');
 var prismicEndpoint = 'https://looseleashdog.prismic.io/api/v2';
+const secure = require("ssl-express-www");
+
 
 const app = express();
-
+app.use(secure);
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Prismic
 // Standardized URLs for known types
@@ -114,11 +119,87 @@ app.post('/contact', function (req, res) {
 
     let ifError = false;
 
-    const { user_name, user_email, message } = req.body;
+    const {
+        user_name,
+        user_email,
+        message
+    } = req.body;
 
-    module.exports = { user_email, user_name, message };
+    // Mailer transport object 
+    var transporter = nodemailer.createTransport({
+        host: 'mi3-ts3.a2hosting.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+        }
+    });
 
-    const { transporter, inquiry, finalConfirm } = require('./nodemailer.js');
+    // Templates
+    function inquiryTemplate() {
+
+        // Do not remove backtick
+        let inqTemplate = `
+
+        <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
+            <h2>Hi Emily, someone is reaching out from your website!</h2>
+                <p>Below is a copy of the email.</p> 
+            <h2>From:</h2>
+                <p style='padding: 0 30px;'><strong>${user_name}</strong></p>
+            <h2>Email:</h2>
+                <p style='padding: 0 30px;'>${user_email}</p>
+            <h2>Message:</h2>
+                <p style='padding: 0 30px;'>${message}</p>
+        </div>
+    
+        `; // Do not remove backtick
+
+        let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+        return output;
+    };
+
+    function confirmTemplate() {
+
+        // Do not remove backtick
+        let confTemplate = `
+
+        <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
+            <h2>Hi ${user_name}, thanks for reaching out to LooseLeashDog!</h2>
+                <p>This is an automatic response confirming that your email was sent. I will reach out to you within the next few days. Below is a copy of your email.</p> 
+                <p>Remember, this is an automatic email and doesn't accept replys.</p>
+            <h2>From:</h2>
+                <p style='padding: 0 30px;'><strong>${user_name}</strong></p>  
+            <h2>Email:</h2>
+                <p style='padding: 0 30px;'>${user_email}</p>
+            <h2>Message:</h2>
+                <p style='padding: 0 30px;'>${message}</p>
+        </div>
+
+        `; // Do not remove backtick
+
+        let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+        return output;
+    };
+
+    // Nodemailer email objects
+    function mailNewInquiry(user_name, user_email, message) {
+        return `{"from": "mailer@looseleashdog.com",
+    "to": "ashthomasweb@gmail.com",
+    "subject": "A person from your website is reaching out!",
+    "html": "${inquiryTemplate()}"}`;
+    };
+
+    function mailConfirmation(user_name, user_email, message) {
+        return `{"from": "mailer@looseleashdog.com",
+    "to": "${user_email}",
+    "subject": "This is your email confirmation from LooseLeashDog!",
+    "html": "${confirmTemplate()}"}`;
+    };
+
+    // Object parsing
+    let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
+    let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
 
     var userInquiry = transporter.sendMail(inquiry);
 
@@ -144,16 +225,10 @@ app.post('/contact', function (req, res) {
 
 // || Listener 
 
-// const server = app.listen(0, () => {
-//     console.log('Server running at port:', server.address().port);
-// });
-
-
 let port = process.env.PORT;
-// if (port == null || port == "") {
-//     port = 3000;
-// }
+if (port == null || port == "") {
+    port = 3000;
+}
 app.listen(port, () => console.log(`Server started at port ${port}.`));
-
 
 // || END of document
--- nodemailer.js
+++ nodemailer.js
@@ -1,88 +1,88 @@
-// Nodemailer functions and templates for "Looseleashdog" 
+// // Nodemailer functions and templates for "Looseleashdog" 
 
-// Dependencies
-const nodemailer = require("nodemailer");
-const { user_name, user_email, message, } = require('./app.js');
+// // Dependencies
+// const nodemailer = require("nodemailer");
+// const { user_name, user_email, message, } = require('./app.js');
 
-// Mailer transport object 
-var transporter = nodemailer.createTransport({
-    host: 'mi3-ts3.a2hosting.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-    }
-});
+// // Mailer transport object 
+// var transporter = nodemailer.createTransport({
+//     host: 'mi3-ts3.a2hosting.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS,
+//     }
+// });
 
-// Templates
-function inquiryTemplate() {
+// // Templates
+// function inquiryTemplate() {
 
-    // Do not remove backtick
-    let inqTemplate = `
+//     // Do not remove backtick
+//     let inqTemplate = `
 
-    <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
-        <h2>Hi Emily, someone is reaching out from your website!</h2>
-            <p>Below is a copy of the email.</p> 
-        <h2>From:</h2>
-            <p style='padding: 0 30px;'><strong>${user_name}</strong></p>
-        <h2>Email:</h2>
-            <p style='padding: 0 30px;'>${user_email}</p>
-        <h2>Message:</h2>
-            <p style='padding: 0 30px;'>${message}</p>
-    </div>
+//     <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>
+//         <h2>Hi Emily, someone is reaching out from your website!</h2>
+//             <p>Below is a copy of the email.</p> 
+//         <h2>From:</h2>
+//             <p style='padding: 0 30px;'><strong>${user_name}</strong></p>
+//         <h2>Email:</h2>
+//             <p style='padding: 0 30px;'>${user_email}</p>
+//         <h2>Message:</h2>
+//             <p style='padding: 0 30px;'>${message}</p>
+//     </div>
     
-    `; // Do not remove backtick
+//     `; // Do not remove backtick
 
-    let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-    return output;
-};
+//     let output = inqTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+//     return output;
+// };
 
-function confirmTemplate() {
+// function confirmTemplate() {
 
-    // Do not remove backtick
-    let confTemplate = `
+//     // Do not remove backtick
+//     let confTemplate = `
 
-    <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
-        <h2>Hi ${user_name}, thanks for reaching out to LooseLeashDog!</h2>
-            <p>This is an automatic response confirming that your email was sent. I will reach out to you within the next few days. Below is a copy of your email.</p> 
-            <p>Remember, this is an automatic email and doesn't accept replys.</p>
-        <h2>From:</h2>
-            <p style='padding: 0 30px;'><strong>${user_name}</strong></p>  
-        <h2>Email:</h2>
-            <p style='padding: 0 30px;'>${user_email}</p>
-        <h2>Message:</h2>
-            <p style='padding: 0 30px;'>${message}</p>
-    </div>
+//     <div style='max-width: 80%; padding: 30px; border: 1px solid lightgrey; border-radius: 12px; margin: 15px;'>   
+//         <h2>Hi ${user_name}, thanks for reaching out to LooseLeashDog!</h2>
+//             <p>This is an automatic response confirming that your email was sent. I will reach out to you within the next few days. Below is a copy of your email.</p> 
+//             <p>Remember, this is an automatic email and doesn't accept replys.</p>
+//         <h2>From:</h2>
+//             <p style='padding: 0 30px;'><strong>${user_name}</strong></p>  
+//         <h2>Email:</h2>
+//             <p style='padding: 0 30px;'>${user_email}</p>
+//         <h2>Message:</h2>
+//             <p style='padding: 0 30px;'>${message}</p>
+//     </div>
 
-    `; // Do not remove backtick
+//     `; // Do not remove backtick
 
-    let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
-    return output;  
-};
+//     let output = confTemplate.replace(/\n/g, "").replace(/\r/g, "<br>");
+//     return output;  
+// };
 
-// Nodemailer email objects
-function mailNewInquiry(user_name, user_email, message) {
-    return `{"from": "mailer@looseleashdog.com",
-    "to": "ashthomasweb@gmail.com",
-    "subject": "A person from your website is reaching out!",
-    "html": "${inquiryTemplate()}"}`;
-};
+// // Nodemailer email objects
+// function mailNewInquiry(user_name, user_email, message) {
+//     return `{"from": "mailer@looseleashdog.com",
+//     "to": "ashthomasweb@gmail.com",
+//     "subject": "A person from your website is reaching out!",
+//     "html": "${inquiryTemplate()}"}`;
+// };
 
-function mailConfirmation(user_name, user_email, message) {
-    return `{"from": "mailer@looseleashdog.com",
-    "to": "${user_email}",
-    "subject": "This is your email confirmation from LooseLeashDog!",
-    "html": "${confirmTemplate()}"}`;
-};
+// function mailConfirmation(user_name, user_email, message) {
+//     return `{"from": "mailer@looseleashdog.com",
+//     "to": "${user_email}",
+//     "subject": "This is your email confirmation from LooseLeashDog!",
+//     "html": "${confirmTemplate()}"}`;
+// };
 
-// Object parsing
-let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
-let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
+// // Object parsing
+// let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
+// let finalConfirm = JSON.parse(mailConfirmation(user_name, user_email, message));
 
-// Exports
-module.exports = {
-    transporter,
-    inquiry,
-    finalConfirm,
-};
+// // Exports
+// module.exports = {
+//     transporter,
+//     inquiry,
+//     finalConfirm,
+// };
