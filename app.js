241cd0329fd0f0d95f5da512e1c0d3480e0abcc6
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-10-16 17:28:24 -0400

    WIP - nodemailer setup live test

--- app.js
+++ app.js
@@ -129,7 +129,6 @@ app.post('/contact', async (req, res) => {
 
     // Mailer transport object 
 
-    
     const oauth2Client = new OAuth2(
         process.env.CLIENT_ID,
         process.env.CLIENT_SECRET,
@@ -153,50 +152,7 @@ app.post('/contact', async (req, res) => {
             accessToken: accessToken
         }
     });
-   
-    // const createTransporter = async () => {
-    //     const oauth2Client = new OAuth2(
-    //         process.env.CLIENT_ID,
-    //         process.env.CLIENT_SECRET,
-    //         "https://developers.google.com/oauthplayground"
-    //     );
-
-    //     oauth2Client.setCredentials({
-    //         refresh_token: process.env.REFRESH_TOKEN
-    //     });
-
-    //     const accessToken = await new Promise((resolve, reject) => {
-    //         oauth2Client.getAccessToken((err, token) => {
-    //             if (err) {
-    //                 reject();
-    //             }
-    //             resolve(token);
-    //         });
-    //     });
-
-    //     const transporter = nodemailer.createTransport({
-    //         service: "gmail",
-    //         auth: {
-    //             type: "OAuth2",
-    //             user: process.env.EMAIL,
-    //             accessToken,
-    //             clientId: process.env.CLIENT_ID,
-    //             clientSecret: process.env.CLIENT_SECRET,
-    //             refreshToken: process.env.REFRESH_TOKEN
-    //         }
-    //     });
-
-    //     return transporter;
-
-
-
-    // };
-
-
-
-
-
-
+  
     // Templates
     function inquiryTemplate() {
 
@@ -255,12 +211,6 @@ app.post('/contact', async (req, res) => {
                 "subject": "This is your email confirmation from LooseLeashDog!",
                 "html": "${confirmTemplate()}"}`;
     };
-
-    //emailOptions - who sends what to whom
-    // const sendEmail = async (emailOptions) => {
-    //     let emailTransporter = await createTransporter();
-    //     await emailTransporter.sendMail(emailOptions);
-    // };
 
     // // Object parsing
     let inquiry = JSON.parse(mailNewInquiry(user_name, user_email, message));
